0df7cda04f86e18c31d99db483b9dc098ab084eb
thunder011 <30837445+thunder011@users.noreply.github.com>
2017-11-26 21:02:12 -0800

    Spot actualizado
    Se cambio añadio la posibilidad de añadir una parada

--- webapp/apis/spot.php
+++ webapp/apis/spot.php
@@ -3,9 +3,11 @@
   // allow access from aoutside the server
   header('Access-Control-Allow-Origin: *');
   // alloww Methods
-  header('Access-Control-Allow-Methods: GET, PUT');
+  header('Access-Control-Allow-Methods: GET, POST, PUT');
 
   require_once($_SERVER['DOCUMENT_ROOT'].'/sharemycar/webapp/models/spot.php');
+  require_once($_SERVER['DOCUMENT_ROOT'].'/sharemycar/webapp/models/studentdriver.php');
+  require_once($_SERVER['DOCUMENT_ROOT'].'/sharemycar/webapp/models/location.php');
 
   // GET
   if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -29,5 +31,59 @@
     }
 
 
+  }
+
+
+  //POST
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+  {
+    if (isset($_POST['student']) &&
+        isset($_POST['slot']) &&
+        isset($_POST['latitude']) &&
+        isset($_POST['longitude']) ) 
+    {
+     try 
+      {
+        // create object
+        $s = new StudentDriver($_POST['student']);
+
+      } 
+      catch (RecordNotFoundException $ex) 
+      {
+          echo json_encode(array(
+            'status' => 2,
+            'errorMessage' => $ex->get_message()
+          ));
+      }
+
+      $sp = new Spot();
+      $sp->setStudent($s);
+      $sp->setLocation(new Location($_POST['latitude'], $_POST['longitude']));
+      $sp->setSlot($_POST['slot']);
+      //add
+      if ($sp->add())
+      {
+          echo json_encode(array(
+              'status' => 0,
+              'errorMessage' => 'Spot added successfully'
+            ));
+
+      }
+      else
+      {
+          echo json_encode(array(
+              'status' => 3,
+              'errorMessage' => 'Could not add spot'
+            ));
+
+      }
+    } 
+    else 
+    {
+      echo json_encode(array(
+              'status' => 1,
+              'errorMessage' => 'Missing parameters'
+            ));
+    }
   }
 ?>
