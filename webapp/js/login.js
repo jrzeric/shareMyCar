bd872a4eae3ea5e407ffea077a90c5429424b314
thunder011 <30837445+thunder011@users.noreply.github.com>
2017-11-26 21:06:17 -0800

    Todos los JS mas actuales
    Los JS contienen metodos de
    Para hacer el login 
    Para desplegar los datos guardados en la sesion
    Para registrar ambos tipos de usuarios
    Para añadir puntos en el mapa y guardarlos en el arreglo

---
+++ webapp/js/login.js
@@ -0,0 +1,58 @@
+function login() {
+	console.log('Getting token...');
+	var x = new XMLHttpRequest();
+	x.open('GET', 'http://localhost:8080/sharemycar/webapp/apis/login.php', true);
+	x.setRequestHeader('email', document.getElementById('email').value);
+	x.setRequestHeader('password', document.getElementById('password').value);
+	x.send();
+	x.onreadystatechange = function() {
+		if (x.readyState == 4 && x.status == 200) {
+			var JSONdata = JSON.parse(x.responseText); console.log(JSONdata);
+			if (JSONdata.status == 0) {
+				sessionStorage.authenticated = true;
+				if (JSONdata.user.id.role.id == 'P') 
+				{
+					sessionStorage.userId = JSONdata.user.id.id;
+					sessionStorage.userRole = JSONdata.user.id.role.name;
+					sessionStorage.userName = JSONdata.user.id.name;
+					sessionStorage.userLastName = JSONdata.user.id.lastName;
+					sessionStorage.userSecondLastName = JSONdata.user.id.secondLastName;
+					sessionStorage.userUniversity = JSONdata.user.id.university.name;
+					sessionStorage.userUniversityLat = JSONdata.user.id.university.location.latitude;
+					sessionStorage.userUniversityLon = JSONdata.user.id.university.location.longitude;
+					sessionStorage.userLocationLat = JSONdata.user.id.location.latitude;
+					sessionStorage.userLocationLon = JSONdata.user.id.location.longitude;
+					sessionStorage.userControlNumber = JSONdata.user.id.controlNumber;
+					sessionStorage.token = JSONdata.token;
+				}
+				else
+				{
+					sessionStorage.userId = JSONdata.user.id.id;
+					sessionStorage.userRole = JSONdata.user.id.role.name;
+					sessionStorage.userName = JSONdata.user.id.name;
+					sessionStorage.userLastName = JSONdata.user.id.lastName;
+					sessionStorage.userSecondLastName = JSONdata.user.id.secondLastName;
+					sessionStorage.userUniversity = JSONdata.user.id.university.name;
+					sessionStorage.userUniversityLat = JSONdata.user.id.university.location.latitude;
+					sessionStorage.userUniversityLon = JSONdata.user.id.university.location.longitude;
+					sessionStorage.userLocationLat = JSONdata.user.id.location.latitude;
+					sessionStorage.userLocationLon = JSONdata.user.id.location.longitude;
+					sessionStorage.userControlNumber = JSONdata.user.id.controlNumber;
+					sessionStorage.userCarBrand = JSONdata.user.id.car.brand.name;
+					sessionStorage.userCarModel = JSONdata.user.id.car.model.name;
+					sessionStorage.userYear = JSONdata.user.id.car.year;
+					sessionStorage.userLicensePlate = JSONdata.user.id.car.licensePlate;
+					sessionStorage.userDriverLicense = JSONdata.user.id.car.driverLicense;
+					sessionStorage.token = JSONdata.token;
+				}
+				console.log(sessionStorage.userUniversityLon);
+				console.log(JSONdata.user.id.university.location.longitude);
+				window.location = 'home.html';
+			}
+			else{
+				document.getElementById('error').innerHTML = JSONdata.errorMessage;
+			}
+		}
+	}
+	
+}
---
+++ webapp/js/registerSpots.js
@@ -0,0 +1,143 @@
+var map;
+var markers = [];
+var coordinates = [[sessionStorage.userLocationLat, sessionStorage.userLocationLon]];
+var valid = 0;
+
+function init() 
+{
+
+  var home = new google.maps.LatLng(sessionStorage.userLocationLat, sessionStorage.userLocationLon);
+  var university = new google.maps.LatLng(sessionStorage.userUniversityLat, sessionStorage.userUniversityLon);
+
+  map = new google.maps.Map(document.getElementById('map'), 
+  {
+    zoom: 10,
+    center: new google.maps.LatLng(sessionStorage.userLocationLat, sessionStorage.userLocationLon),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // This event listener will call addMarker() when the map is clicked.
+  map.addListener('click', function(event) 
+  {
+    addMarker(event.latLng);
+  });
+
+  // Adds a marker at the center of the map.
+  addHome(home);
+  addUniversity(university);
+}
+
+// Adds a marker to the map and push to the array.
+function addHome(location) 
+{
+  var marker = new google.maps.Marker(
+  {
+    position: location,
+    map: map
+  });
+}
+
+// Adds a marker to the map and push to the array.
+function addUniversity(location) 
+{
+  var marker = new google.maps.Marker(
+  {
+    position: location,
+    map: map
+  });
+}
+
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) 
+{
+  if (valid < 6) 
+  {
+    valid++;
+      var marker = new google.maps.Marker(
+    {
+      position: location,
+      map: map
+    });
+    markers.push(marker);
+    var coordinate = [location.lat(), location.lng()];
+    coordinates.push(coordinate);
+    console.log(location.lat() + ', ' + location.lng())
+    console.log(coordinates);
+    console.log(markers);
+    alert(location);
+  }
+  else
+  {
+    alert('No puedes agregar mas puntos de encuentro');
+  }
+
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) 
+{
+  for (var i = 0; i < markers.length; i++) { markers[i].setMap(map); }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() 
+{
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() 
+{
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() 
+{
+  clearMarkers();
+  markers = [];
+  valid = 0;
+}
+
+function registerSpots()
+{
+  console.log('POSTING spots...');
+  for (var i = 0; i <= valid; i++) 
+  {
+    //create request
+    var x = new XMLHttpRequest();
+    //prepare request
+    console.log(sessionStorage.userId);
+    x.open('POST', 'http://localhost:8080/sharemycar/webapp/apis/spot.php', true);
+    //form data
+    var fd = new FormData();
+    //values
+    fd.append('student', sessionStorage.userId);
+    fd.append('slot', i);
+    fd.append('latitude', coordinates[i][0]);
+    fd.append('longitude', coordinates[i][1]);
+
+    console.log(fd);
+    //send
+    x.send(fd);
+    console.log(fd);
+    //handle readyState change event
+    x.onreadystatechange = function() 
+    {
+      // check status
+      // status : 200=OK, 404=Page not found, 500=server denied access
+      // readyState : 4=Back with data
+      if (x.status == 200 && x.readyState == 4) 
+      {
+
+        var JSONdata = JSON.parse(x.responseText); console.log(JSONdata);
+        alert(JSONdata.errorMessage);
+        //show buildings
+        console.log(x.responseText);
+      }
+    }
+
+  }
+
+}
