2588c1323cae183aee4395a3691c4edb632529dc
thunder011 <30837445+thunder011@users.noreply.github.com>
2018-01-22 21:08:58 -0800

    Comments added to apis spot, brand, studentpassengerinsert
    Comments added to apis spot, brand, studentpassengerinsert

--- webapp/apis/brand.php
+++ webapp/apis/brand.php
@@ -9,18 +9,21 @@
 
 	//GET (Read)
 	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-		//parameters
+		/*Verify that id is declared*/
 		if (isset($_GET['id'])) {
 			try {
-				//create object
+				/*Create an object Spot to verify that exist*/
 				$b = new Brand($_GET['id']);
 				//display
+				/*display the object created previously*/
 				echo json_encode(array(
 					'status' => 0,
 					'brand' => json_decode($b->toJson())
 				));
 			}
+			/*If doesn't exist so the constructor throws an exception and catch that and show the error*/
 			catch (RecordNotFoundException $ex) {
+				/*the error is caused because the id ingresed doesn't exist in the database*/
 				echo json_encode(array(
 					'status' => 1,
 					'errorMessage' => $ex->get_message()
@@ -28,6 +31,7 @@
 			}
 		}
 		else {
+			/*if we don't recive paramerts will show all brands*/
 			echo Brand::getAllJson();
 		}
 
