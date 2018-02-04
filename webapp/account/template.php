40310058d2979c0de90325524003104167841768
Eric Juarez <eric.jrz@gmail.com>
2018-02-04 03:10:46 -0800

    Se eliminan los archivos: 'archivoPrueba.txt' y 'prueba 2.txt'. Se anade plantilla en el directorio webapp/account/*

---
+++ webapp/account/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title><?php require_once('lib/title.php'); ?> Template </title>
+  </head>
+  <body>
+    <header>
+      <?php require_once('lib/header.php'); ?>
+    </header>
+    <main>
+      <h1>Title</h1>
+    </main>
+    <footer>
+      <?php require_once('lib/footer.php'); ?>
+    </footer>
+  </body>
+</html>
