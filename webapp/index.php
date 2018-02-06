32c3a92df4708b1c09c61c0206fa4e8ac349240d
Eric Juarez <eric.jrz@gmail.com>
2018-02-06 01:19:21 -0800

    Se restructura webapp/index.php (de visita)

---
+++ webapp/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Share My Car</title>
+		<link href="css/style.css" rel="stylesheet"></link>
+		<link href="css/footer.css" rel="stylesheet"></link>
+	</head>
+	<body>
+    <header>
+      <img class="header__logo" src="images/logo.png" alt="">
+      <img class="header__download-app-icon" src="images/download-app-02.png" alt="">
+      <button class="header__button-login" type="button" name="login">Login</button>
+      <a href="register1.html"><button class="header__button-register" type="button" name="register">Register</button></a>
+    </header>
+    <main>
+      <div class="presentation">
+        <div class="description-box">
+          <label class="description-box__title">Title</label>
+          <p class="description-box__paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div class="image-box">
+          <img class="image-box__university-logo" src="images/utt.png" alt="">
+          <img class="image-box__university-logo" src="images/itt.png" alt="">
+          <img class="image-box__university-logo" src="images/uabc.png" alt="">
+          <img class="image-box__university-logo" src="images/cetys.png" alt="">
+          <img class="image-box__university-logo" src="images/xochi.png" alt="">
+          <img class="image-box__university-logo" src="images/unidep.png" alt="">
+        </div>
+      </div>
+      <div class="information">
+        <div class="column-information">
+          <label class="column-information__title">Title</label>
+          <p class="column-information__paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <img class="column-information__image" src="images/sample.png" alt="">
+        </div>
+        <div class="column-information">
+          <label class="column-information__title">Title</label>
+          <p class="column-information__paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <img class="column-information__image" src="images/sample.png" alt="">
+        </div>
+        <div class="column-information">
+          <label class="column-information__title">Title</label>
+          <p class="column-information__paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <img class="column-information__image" src="images/sample.png" alt="">
+        </div>
+      </div>
+		</main>
+  </body>
+  <footer>
+    <?php require_once("lib/footer.php"); ?>
+  </footer>
+</html>
