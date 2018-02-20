ff5a41eb298ab63107cd2942b9af9378459bfbeb
tilo <tiloanne96@gmail.com>
2018-02-19 19:44:22 -0800

    leave the changes on template.php, I created a file dashboard.php for the admin

---
+++ webapp/account/admin/dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Estilos-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <title> Template </title>
+  </head>
+  <body>
+    <header>
+      <?php require_once($_SERVER['DOCUMENT_ROOT']."/lib/header_users.php"); ?>
+		</header>
+
+  </body>
+</html>
